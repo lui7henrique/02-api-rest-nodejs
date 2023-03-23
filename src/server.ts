3cdaa9caf6263c687d1ef514a327cb3fb773282d
Luiz Henrique <7henrique18@gmail.com>
2023-03-22 21:49:20 -0300

    feat: Making queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transactions = [
+  //   await knex('transactions').insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   }),
+  //   await knex('transactions').insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   }),
+  // ]
 
-  return tables
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
