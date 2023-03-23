3ead0cbe193eeab954d592dc971486487081cede
Luiz Henrique <7henrique18@gmail.com>
2023-03-23 20:16:06 -0300

    feat: Fastify plugin

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const transactions = [
-  //   await knex('transactions').insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   }),
-  //   await knex('transactions').insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   }),
-  // ]
-
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
